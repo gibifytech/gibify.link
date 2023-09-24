470d64915377003cc76cf274852b5d64af6f2e7c
gibify <gibifyofficial@gmail.com>
2023-09-24 20:10:40 -0300

    alexandre adjuts

--- src/app/alexandre.official.brasil/page.tsx
+++ src/app/alexandre.official.brasil/page.tsx
@@ -1,32 +1,33 @@
 import GithubIcon from "@/components/icons/github";
+import InstagramIcon from "@/components/icons/instagram";
 import WhtasappIcon from "@/components/icons/whatsapp";
 import { Metadata } from "next";
 import Image from "next/image";
 
 export const metadata: Metadata = {
   title: 'Gibify Link | Alexandre Brasil',
-  description: 'Simplificando a maneira de como você compartilha seus links na internet.',
+  description: 'Simplificando a maneira de compartilhar seus links na internet.',
 }
 export default function AlexandreOfficialBrasilPage() {
   return (
     <main className="flex max-w-5xl w-full min-h-screen flex-col items-center justify-start p-5 mx-auto">
       <div className='flex gap-2 items-center mb-4'>
         <Image
-          className="w-10 h-10 p-1 rounded-full ring-2 ring-green-500/70"
+          className="w-10 h-10 p-1 rounded-full ring-2 ring-green-500"
           src="https://instagram.fnvt1-1.fna.fbcdn.net/v/t51.2885-19/361890449_183801664680630_8733781334214549013_n.jpg?stp=dst-jpg_s320x320&_nc_ht=instagram.fnvt1-1.fna.fbcdn.net&_nc_cat=106&_nc_ohc=uZGBv9jmoQAAX-Hs4-D&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AfAcKCgVUYWbP7wOCMsz2l8SEQZKvheaRw2W1sqr8V9TOQ&oe=650F6FB1&_nc_sid=8b3546"
           alt="Bordered avatar"
           width={10}
           height={10}
         />
-        <span className='text-black'>Alexandre Brasil</span>
+        <span className='text-black dark:text-white'>Alexandre Brasil</span>
       </div>
       <div className="max-w-sm w-full mt-6">
         <ul>
           <li>Web Developer</li>
-          <li>Apaixonado por desenvolver aplicações</li>
+          <li>Apaixonado por  Programar</li>
         </ul>
       </div>
-      <div className="flex flex-col gap-4 max-w-sm w-full mt-6">
+      <div className="flex flex-col gap-4 max-w-sm w-full mt-6 md:text-base text-sm">
         <a
           href="https://github.com/alexandre-cooper"
           target="_blank"
@@ -34,7 +35,7 @@ export default function AlexandreOfficialBrasilPage() {
           className="flex items-center justify-center gap-2 rounded-3xl border border-green-500 p-2 w-full"
         >
           <GithubIcon />
-          <span className="md:text-base text-sm">Github</span>
+          <span className="">Github</span>
         </a>
         <a
           href="https://wa.me/5565992792383"
@@ -44,6 +45,15 @@ export default function AlexandreOfficialBrasilPage() {
         >
           <WhtasappIcon />
           <span className="md:text-base text-sm">WhatsApp</span>
+        </a>
+        <a
+          href="https://www.instagram.com/alexandre.official.brasil/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center justify-center gap-2 rounded-3xl border border-green-500 p-2 w-full"
+        >
+          <InstagramIcon />
+          <span className="md:text-base text-sm">Instagram</span>
         </a>
       </div>
     </main>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,35 +1,40 @@
+import WhtasappIcon from '@/components/icons/whatsapp'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-10 max-w-5xl mx-auto">
+    <main className="flex min-h-screen flex-col items-center justify-start p-6 md:p-10 max-w-5xl mx-auto">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <Image
           src="/gibify.link.svg"
           alt="Logo da Gibidy Link"
           className="dark:invert"
-          width={300}
+          width={160}
           height={60}
           priority
         />
       </div>
 
 
-      <h1 className='text-3xl md:text-6xl'>Simplificando a maneira de você compartilhar seu conteúdo na internet.</h1>
-      <p>
-        O <i>Gibify Link</i> é a ferramenta ideal para influenciadores, criadores de conteúdo, empresas e qualquer pessoa que deseja otimizar sua presença online.
-        É hora de simplificar a maneira como você compartilha seu conteúdo na web.
-        Junte-se ao <i>Gibify Link</i> e crie seu link personalizado hoje mesmo!
-      </p>
+      <h1 className='text-3xl md:text-6xl font-medium mt-8'>
+        Simplificando a maneira de compartilhar seus links na internet.
+      </h1>
+      <h3 className='md:text-xl text-lg text-black/70 mt-8'>
+        O <i>Gibify Link</i> é a ferramenta ideal para influenciadores, criadores de conteúdo, e qualquer pessoa que deseja otimizar sua presença online.
+        É hora de simplificar a maneira de compartilhar seu conteúdo na internet.
+        Junte-se a <i>Gibify Link</i> e crie seu link personalizado hoje mesmo!
+      </h3>
 
-      <div className="mb-32 grid gap-2 text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4">
+      <div className="mt-8 border-l-2 pl-4">
+        <p className='text-black/60'>Fale com um dos nossos Ddesenvolvedores e cria seu link com a hospedagem, SSL e domínio totalmente grátis.</p>
         <a
-          href=""
-          className="group rounded-lg border border-transparent uppercase text-lg font-semibold p-2 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          href="https://wa.me/5565992792383"
           target="_blank"
           rel="noopener noreferrer"
+          className="flex max-w-sm mt-4 items-center justify-center gap-2 rounded-3xl border border-green-500 p-2 w-full"
         >
-          whatsapp
+          <WhtasappIcon />
+          <span className="md:text-base text-sm">WhatsApp</span>
         </a>
       </div>
     </main>
