9042f5f0a493b6dfd3e9c27f4c8751493fc81888
gibify <gibifyofficial@gmail.com>
2023-09-25 14:06:02 -0300

    feta: add vanubia links

---
+++ src/app/vanubia_xavier_/page.tsx
@@ -0,0 +1,54 @@
+import InstagramIcon from "@/components/icons/instagram";
+import WhtasappIcon from "@/components/icons/whatsapp";
+import { Metadata } from "next";
+import Image from "next/image";
+
+export const metadata: Metadata = {
+  title: 'Gibify Link | Vanubia Xavier',
+  description: 'Simplificando a maneira de compartilhar seus links na internet.',
+}
+export default function VanubiaXavierPage() {
+  return (
+    <main className="flex max-w-5xl w-full min-h-screen flex-col items-center justify-start p-5 mx-auto">
+      <div className='flex gap-2 items-center justify-start mt-6'>
+        <Image
+          className="w-[70px] h-[70px] p-1 rounded-full ring-2 ring-[#8F6566]"
+          src="https://instagram.fnvt1-1.fna.fbcdn.net/v/t51.2885-19/296612066_623545722520792_3226160538313054067_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.fnvt1-1.fna.fbcdn.net&_nc_cat=104&_nc_ohc=ZbmaS8A2P1QAX_ClswH&edm=ACWDqb8BAAAA&ccb=7-5&oh=00_AfBNcAsvBBqfIaaUcIwh0zZSLC3sVjWxQ3oYkoqtKTt5vQ&oe=65164795&_nc_sid=ee9879"
+          alt="Bordered avatar"
+          width={75}
+          height={75}
+        />
+        <span className='text-black dark:text-white md:text-lg text-base'>Vanubia Xavier</span>
+      </div>
+      <div className="max-w-sm w-full mt-6 my-4">
+        <ul>
+          <li>
+            Esteticista apaixonada pela área capilar!
+            Transformando você na sua melhor versão!
+            Lembre-se sempre : Se nada mudar! mude você.
+          </li>
+        </ul>
+      </div>
+      <div className="flex flex-col gap-4 max-w-sm w-full mt-6 md:text-base text-sm">
+        <a
+          href="https://wa.me/5565992700066"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center justify-center gap-2 rounded-3xl text-[#8F6566] bg-[#EFD8D2] border border-[#8F6566] p-2 w-full"
+        >
+          <WhtasappIcon />
+          <span className="md:text-base text-sm">WhatsApp</span>
+        </a>
+        <a
+          href="https://www.instagram.com/vanubia_xavier_/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="flex items-center justify-center gap-2 rounded-3xl text-[#8F6566] bg-[#EFD8D2] border border-[#8F6566] p-2 w-full"
+        >
+          <InstagramIcon />
+          <span className="md:text-base text-sm">Instagram</span>
+        </a>
+      </div>
+    </main>
+  )
+} 
--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -5,13 +5,16 @@ export default function Footer() {
 
   return (
     <footer className="w-full border-t p-4 text-center text-black dark:text-white">
-      <p> &copy; {copyrightDate} Todos os direitos reservados.</p>
-      <a
-        href="https://gibify.com.br"
-        className="text-black/70  cursor-pointer"
-      >
-        App da <strong>Gibify -  Marketing e Technologia</strong>
-      </a>
+      <p> &copy; {copyrightDate} Todos os direitos reservados por <a href="https://gibify.link" className="text-black/70 font-semibold">Gibify Link</a></p>
+      <p>
+        App Desenvolvido pela <br />
+        <a
+          href="https://gibify.com.br"
+          className="text-black/70 font-semibold"
+        >
+          Gibify -  Marketing e Technologia
+        </a>
+      </p>
     </footer>
   )
 }
