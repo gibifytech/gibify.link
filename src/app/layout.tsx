751e61886ebdcd6ccf5f346358633b52c57b7b63
gibify <gibifyofficial@gmail.com>
2023-09-27 21:01:29 -0300

    fix: facebook-domain-verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,11 +15,11 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: 'Gibify Link',
   description: 'Simplificando a maneira de compartilha seus links na internet.',
-  other: {
-    facebook: 'alnqlc2yhr8rblyge29wouyoon2483',
-  },
   verification: {
     google: 'NZ-zRsj3eU2EdmeG8llgzrBIHSozkP7W9illfOmVr0M',
+    other: {
+      'facebook-domain-verification': 'alnqlc2yhr8rblyge29wouyoon2483',
+    }
   }
 }
 
