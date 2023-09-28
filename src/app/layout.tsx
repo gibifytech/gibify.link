6118caa5bd5dfd9811083a682228e71f5500900d
gibify <gibifyofficial@gmail.com>
2023-09-28 10:17:48 -0300

    feat: (facebook) add pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { ThemeProvider } from './theme-provider';
 import Link from 'next/link';
+import Script from 'next/script';
 
 const inter = Inter({
   subsets: ['latin'],
@@ -31,6 +32,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className={inter.variable}>
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+                  !function(f,b,e,v,n,t,s)
+                  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                  n.queue=[];t=b.createElement(e);t.async=!0;
+                  t.src=v;s=b.getElementsByTagName(e)[0];
+                  s.parentNode.insertBefore(t,s)}(window, document,'script',
+                  'https://connect.facebook.net/en_US/fbevents.js');
+                  fbq('init', '556848779904334');
+                  fbq('track', 'PageView');
+                  `,
+        }}
+      />
       <body>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <Header />
