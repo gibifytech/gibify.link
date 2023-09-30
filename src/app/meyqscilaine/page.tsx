0f37ecb17f15a14857dd55570b40bf41f3f14de4
gibify <gibifyofficial@gmail.com>
2023-09-30 16:01:30 -0300

    fix: (meyqscilaine) image size

--- src/app/meyqscilaine/page.tsx
+++ src/app/meyqscilaine/page.tsx
@@ -13,10 +13,12 @@ export default function MeyqscilainePage() {
       <div className='flex md:flex-row flex-col gap-4 items-center justify-center mt-8  w-full h-[100px]'>
         <Image
           className="w-[75px] h-[75px] p-1 rounded-full ring-2 ring-[#8F6566]"
-          src="https://instagram.fnvt1-1.fna.fbcdn.net/v/t51.2885-19/296612066_623545722520792_3226160538313054067_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.fnvt1-1.fna.fbcdn.net&_nc_cat=104&_nc_ohc=ZbmaS8A2P1QAX_ClswH&edm=ACWDqb8BAAAA&ccb=7-5&oh=00_AfBNcAsvBBqfIaaUcIwh0zZSLC3sVjWxQ3oYkoqtKTt5vQ&oe=65164795&_nc_sid=ee9879"
+          src="/images/meyqscilaine_image.png"
           alt="Bordered avatar"
           width={75}
           height={75}
+          priority
+          objectFit="cover"
         />
         <span className='text-black dark:text-white text-xl'>Meyqscilaine Gonçalves</span>
       </div>
