94295156fffea9a52cd16ec6a676e7308098dbab
gibify <gibifyofficial@gmail.com>
2023-10-01 22:11:58 -0300

    fix(og:image): add metadat base

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,19 @@
 import Carousel from '@/components/carousel'
 import WhtasappIcon from '@/components/icons/whatsapp'
 
+export const metadata = {
+  metadataBase: new URL('https://gibify.link'),
+  alternates: {
+    canonical: '/',
+    languages: {
+      'pt-BR': '/pt-BR'
+    }
+  },
+  openGraph: {
+    images: '/opengraph-image.png'
+  }
+}
+
 export default function HomePage() {
   return (
     <>
