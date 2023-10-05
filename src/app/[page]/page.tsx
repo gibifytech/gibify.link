6d94315c5ff9e2b71a8c02bf22707bdcb4369c72
gibify <gibifyofficial@gmail.com>
2023-10-05 10:08:01 -0300

    fix: import data from .json

--- src/app/[page]/page.tsx
+++ src/app/[page]/page.tsx
@@ -1,19 +1,17 @@
 import Image from 'next/image'
 import Icons from '@/components/icons'
-import json from '../../json/data.json'
+import { data } from '../../json/data.json'
 import { notFound } from 'next/navigation'
 import { Metadata } from 'next'
 
 const { SITE_NAME } = process.env
-export const runtime = 'edge'
-export const revalidate = 43200 // 12 hours in seconds
 
 export async function generateMetadata({
   params
 }: {
   params: { page: string }
 }): Promise<Metadata> {
-  const page = json.data?.find((user) => params.page && user.username === params.page)
+  const page = data?.find((user) => params.page && user.username === params.page)
   if (!page) return notFound()
 
   return {
@@ -23,7 +21,7 @@ export async function generateMetadata({
 }
 
 export default async function Page({ params }: { params: { page: string } }) {
-  const page = json.data?.find((user) => params.page && user.username === params.page)
+  const page = data?.find((user) => params.page && user.username === params.page)
   if (!page) notFound()
 
   return (
