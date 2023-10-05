8198880391781e628179ad20e1969a5d00a8faaa
gibify <gibifyofficial@gmail.com>
2023-10-05 16:15:54 -0300

    feat(mansaohostel): create mansaohostel

--- next.config.js
+++ next.config.js
@@ -7,9 +7,14 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'instagram.fnvt1-1.fna.fbcdn.net',
         pathname: '/v/**'
+      },
+      {
+        protocol: 'https',
+        hostname: 'instagram.fbfh1-2.fna.fbcdn.net',
+        pathname: '/v/**'
       }
     ]
-  },
+  }
 }
 
 module.exports = nextConfig
