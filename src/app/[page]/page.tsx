59fb457eb044d5963fe6fc6d3e2358c902a0d761
gibify <gibifyofficial@gmail.com>
2023-10-05 10:02:09 -0300

    fix: json import

--- src/app/[page]/page.tsx
+++ src/app/[page]/page.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
-import json from '@/json/data.json'
-import { notFound } from 'next/navigation'
+import json from '../../json/data.json'
 import Icons from '@/components/icons'
+import { notFound } from 'next/navigation'
 import { Metadata } from 'next'
 
 const { SITE_NAME } = process.env
