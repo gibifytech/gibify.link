7074c7afac836443ada63c7a36fbd0da322b9ea2
gibify <gibifyofficial@gmail.com>
2023-10-06 11:47:47 -0300

    feat(og:image): add og: image

--- src/app/opengraph-image.tsx
+++ src/app/opengraph-image.tsx
@@ -1,7 +1,79 @@
-import OpengraphImage from '@/components/opengraph-image'
-
+import { ImageResponse } from 'next/server'
 export const runtime = 'edge'
-
-export default async function Image() {
-  return await OpengraphImage()
+export default async function OpengraphImage(): Promise<ImageResponse> {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          height: '100%',
+          width: '100%',
+          display: 'flex',
+          textAlign: 'center',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexDirection: 'column',
+          flexWrap: 'nowrap',
+          backgroundColor: 'white',
+          backgroundImage:
+            'radial-gradient(circle at 25px 25px, lightgray 2%, transparent 0%), radial-gradient(circle at 75px 75px, lightgray 2%, transparent 0%)',
+          backgroundSize: '100px 100px'
+        }}
+      >
+        <div
+          style={{
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center'
+          }}
+        >
+          <svg
+            aria-label="Ícone de link"
+            className="x1lliihq x1n2onr6"
+            fill="current"
+            height="75"
+            role="img"
+            viewBox="0 0 25 25"
+            width="75"
+          >
+            <path
+              d="m9.726 5.123 1.228-1.228a6.47 6.47 0 0 1 9.15 9.152l-1.227 1.227m-4.603 4.603-1.228 1.228a6.47 6.47 0 0 1-9.15-9.152l1.227-1.227"
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+            ></path>
+            <line
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              x1="8.471"
+              x2="15.529"
+              y1="15.529"
+              y2="8.471"
+            ></line>
+          </svg>
+        </div>
+        <div
+          style={{
+            display: 'flex',
+            fontSize: 40,
+            fontStyle: 'normal',
+            color: 'black',
+            marginTop: 30,
+            lineHeight: 1.8,
+            whiteSpace: 'pre-wrap'
+          }}
+        >
+          <b>Gibify Link</b>
+        </div>
+      </div>
+    ),
+    {
+      width: 1200,
+      height: 630
+    }
+  )
 }
