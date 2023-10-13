9e860af42a043c7f38f0a6fefcaad16d1bb264ab
gibify <gibifyofficial@gmail.com>
2023-10-13 19:25:05 -0300

    feat(raffa): create raffa

--- next.config.js
+++ next.config.js
@@ -2,18 +2,7 @@
 const nextConfig = {
   images: {
     formats: ['image/avif', 'image/webp'],
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'instagram.fnvt1-1.fna.fbcdn.net',
-        pathname: '/v/**'
-      },
-      {
-        protocol: 'https',
-        hostname: 'instagram.fbfh1-2.fna.fbcdn.net',
-        pathname: '/v/**'
-      }
-    ]
+    remotePatterns: []
   }
 }
 
